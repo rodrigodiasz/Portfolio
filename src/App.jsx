45b329554aacf4cb1d34c67179e13bd29e1a128f
Rodrigo Oliveira <coderodrigo07@gmail.com>
2025-04-23 20:30:06 -0300

    fix contact deployment

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ import LogoSection from "./sections/LogoSection";
 import FeatureCards from "./sections/FeatureCards";
 import ExperienceSection from "./sections/ExperienceSection";
 import TechStack from "./sections/TechStack"
-import Testimonials from "./sections/Testimonials";
-import Contact from "./sections/ContactSection";
+// import Testimonials from "./sections/Testimonials";
+import Contact from "./sections/Contact";
 import Footer from "./sections/Footer";
 
 const App = () => {
